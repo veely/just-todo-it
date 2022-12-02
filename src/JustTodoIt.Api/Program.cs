e7d74203f883aff40e5aa95726e48d80f8c415d2
Al <ama@touchbistro.com>
2022-12-02 14:38:02 -0500

    Change something

--- src/JustTodoIt.Api/Program.cs
+++ src/JustTodoIt.Api/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! What's up!");
+app.MapGet("/", () => "Hello World! New Change!!!");
 
 app.Run();
